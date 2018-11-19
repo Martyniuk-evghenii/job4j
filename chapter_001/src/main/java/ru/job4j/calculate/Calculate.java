89df8e5eb8ff9989e00c87e2fd5228efbec1833e
Evghenii Martyniuk <martyniuk_evghenii@mail.ua>
2018-11-19 20:15:15 +0200

    1.3. JavaDoc.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate; 
 
+/**
+*Class Calculate решение задачи 001 урок.
+@author Martyniuk Evghenii
+@since 19.11.2008.
+*/
 public class Calculate {
 
+/**
+*Main.
+*@param args - args.
+*/
 	public static void main (String[] args) {
 		System.out.println ("Hello World");
 	}
