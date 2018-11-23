d7fe5c5d0f236824009c210ae947a578c08bbb8e
Evghenii Martyniuk <martyniuk_evghenii@mail.ua>
2018-11-23 13:07:03 +0200

    3.4. Расстояние между точками в системе координат.

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,37 @@
+package ru.job4j.condition;
+
+/**
+ * Расчет расстояния между двумя точками
+ *
+ * @author Martyniuk Evghenii
+ * @version 0.4
+ * @since 0.1
+ */
+public class Point {
+    /*Точка x*/
+    private int x;
+    /*Точка y*/
+    private int y;
+
+    /* Создаем конструктор Point */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Вычисляем расстояние между двумя точками по формуле Декарта
+     *
+     * @param that - конечная точка
+     * @return - расстояние между двумя заданными точками, вычисленное по формуле Декарта
+     */
+    public double distanceTo(Point that) {
+        //Point a = this;
+        // Точка b - это входящая точка. К ней можно обратиться через переменную that.
+        // или для удобства мы создали новую переменную b и к ней присвоили переменную that
+        //Point b = that;
+        return Math.sqrt(
+                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+        );
+    }
+}
